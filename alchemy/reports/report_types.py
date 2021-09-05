b7512cafacb1e445bde35ad806bf50d8b56778cd
RobinT <the5thforce@protonmail.com>
2021-09-05 11:58:03 +0700

    Deleted the commented out old build_self in StudentPaperReport

--- alchemy/reports/report_types.py
+++ alchemy/reports/report_types.py
@@ -21,25 +21,6 @@ class StudentPaperReport(object):
             section = section_class(**self.section_kwargs)
             self.sections.append(section)
 
-
-    # def old_build_self(self):
-    #     self.title = f"{self.student.aws_user.family_name}, {self.student.aws_user.given_name} - Achievment Report"
-    #     self.subtitle = f"{self.clazz.course.name} ({self.clazz.code}): {self.paper.title}"
-    #     overview_section = student_paper_sections.OverviewSection('student/report_section_macros/overview.html', self.student, self.paper)
-    #     self.sections.append(overview_section)
-    #
-    #     adjacent_grades_section = student_paper_sections.AdjacentGradesSection('student/report_section_macros/adjacent_grades.html', self.student, self.paper)
-    #     self.sections.append(adjacent_grades_section)
-    #
-    #     cohort_summary_section = student_paper_sections.CohortSummarySection('student/report_section_macros/cohort_summary.html', self.paper)
-    #     self.sections.append(cohort_summary_section)
-    #
-    #     clazz_summary_section = student_paper_sections.ClazzSummarySection('student/report_section_macros/clazz_summary.html', self.paper, self.clazz)
-    #     self.sections.append(clazz_summary_section)
-    #
-    #     highlights_section = student_paper_sections.HighlightsSection('student/report_section_macros/highlights_section.html', self.student, self.paper)
-    #     self.sections.append(highlights_section)
-
 class ClazzPaperReport(object):
     def __init__(self, clazz, paper):
         self.title = None
