35903b6b62f1217e4782868c0e0d354d16b005e7
RobinT <the5thforce@protonmail.com>
2021-09-05 11:55:47 +0700

    Have replaced the individual creation of sections with a loop - gave all sections the same arguments and passed as a list of keyword args.

--- alchemy/reports/report_types.py
+++ alchemy/reports/report_types.py
@@ -1,42 +1,44 @@
 from alchemy.reports import student_paper_sections, clazz_paper_sections, cohort_paper_sections
 
 class StudentPaperReport(object):
-    def __init__(self, student, clazz, paper, section_types=['OverviewSection', 'AdjacentGradesSection']):
+    def __init__(self, student, clazz, paper, section_types=['OverviewSection', 'AdjacentGradesSection', 'ClazzSummarySection', 'CohortSummarySection', 'HighlightsSection']):
         self.title = None
         self.subtitle = None
         self.student = student
         self.clazz = clazz
         self.paper = paper
+        self.section_kwargs = {}
         self.sections = []
         self.build_self(section_types)
 
     def build_self(self, section_types):
         self.title = f"{self.student.aws_user.family_name}, {self.student.aws_user.given_name} - Achievment Report"
         self.subtitle = f"{self.clazz.course.name} ({self.clazz.code}): {self.paper.title}"
+        self.section_kwargs = {'student': self.student, 'clazz': self.clazz, 'paper': self.paper, 'html_macro': None}
 
         for section_type in section_types:
             section_class = getattr(student_paper_sections, section_type)
-            section = section_class(self.student, self.paper)
+            section = section_class(**self.section_kwargs)
             self.sections.append(section)
 
 
-    def old_build_self(self):
-        self.title = f"{self.student.aws_user.family_name}, {self.student.aws_user.given_name} - Achievment Report"
-        self.subtitle = f"{self.clazz.course.name} ({self.clazz.code}): {self.paper.title}"
-        overview_section = student_paper_sections.OverviewSection('student/report_section_macros/overview.html', self.student, self.paper)
-        self.sections.append(overview_section)
-
-        adjacent_grades_section = student_paper_sections.AdjacentGradesSection('student/report_section_macros/adjacent_grades.html', self.student, self.paper)
-        self.sections.append(adjacent_grades_section)
-
-        cohort_summary_section = student_paper_sections.CohortSummarySection('student/report_section_macros/cohort_summary.html', self.paper)
-        self.sections.append(cohort_summary_section)
-
-        clazz_summary_section = student_paper_sections.ClazzSummarySection('student/report_section_macros/clazz_summary.html', self.paper, self.clazz)
-        self.sections.append(clazz_summary_section)
-
-        highlights_section = student_paper_sections.HighlightsSection('student/report_section_macros/highlights_section.html', self.student, self.paper)
-        self.sections.append(highlights_section)
+    # def old_build_self(self):
+    #     self.title = f"{self.student.aws_user.family_name}, {self.student.aws_user.given_name} - Achievment Report"
+    #     self.subtitle = f"{self.clazz.course.name} ({self.clazz.code}): {self.paper.title}"
+    #     overview_section = student_paper_sections.OverviewSection('student/report_section_macros/overview.html', self.student, self.paper)
+    #     self.sections.append(overview_section)
+    #
+    #     adjacent_grades_section = student_paper_sections.AdjacentGradesSection('student/report_section_macros/adjacent_grades.html', self.student, self.paper)
+    #     self.sections.append(adjacent_grades_section)
+    #
+    #     cohort_summary_section = student_paper_sections.CohortSummarySection('student/report_section_macros/cohort_summary.html', self.paper)
+    #     self.sections.append(cohort_summary_section)
+    #
+    #     clazz_summary_section = student_paper_sections.ClazzSummarySection('student/report_section_macros/clazz_summary.html', self.paper, self.clazz)
+    #     self.sections.append(clazz_summary_section)
+    #
+    #     highlights_section = student_paper_sections.HighlightsSection('student/report_section_macros/highlights_section.html', self.student, self.paper)
+    #     self.sections.append(highlights_section)
 
 class ClazzPaperReport(object):
     def __init__(self, clazz, paper):
--- alchemy/reports/student_paper_sections.py
+++ alchemy/reports/student_paper_sections.py
@@ -2,30 +2,31 @@ from alchemy import models
 from alchemy.reports import data_manager
 
 class StudentReportSection:
-    def __init__(self, html_macro, student, paper):
+    def __init__(self, html_macro, student, clazz, paper):
         self.html_macro = html_macro
         self.student = student
+        self.clazz = clazz
         self.paper = paper
         self.build_self()
 
 class OverviewSection(StudentReportSection):
-    def __init__(self, student, paper):
-        super().__init__('student/report_section_macros/overview.html', student, paper)
+    def __init__(self, html_macro, student, clazz, paper):
+        super().__init__('student/report_section_macros/overview.html', student, clazz, paper)
 
     def build_self(self):
         self.tally = data_manager.PaperScoreTally.from_student(self.student, self.paper)
 
 class AdjacentGradesSection(StudentReportSection):
-    def __init__(self, student, paper):
-        super().__init__('student/report_section_macros/adjacent_grades.html', student, paper)
+    def __init__(self, html_macro, student, clazz, paper):
+        super().__init__('student/report_section_macros/adjacent_grades.html', student, clazz, paper)
 
     def build_self(self):
         tally = data_manager.PaperScoreTally.from_student(self.student, self.paper)
         self.adjacent_grades = data_manager.AdjacentGrades(self.paper.course.grade_levels, tally.percent_total, tally.grade, tally.paper_total)
 
 class ClazzSummarySection(StudentReportSection):
-    def __init__(self, html_macro, paper, clazz):
-        self.html_macro = html_macro
+    def __init__(self, html_macro, student, clazz, paper):
+        self.html_macro = 'student/report_section_macros/clazz_summary.html'
         self.paper = paper
         self.clazz = clazz
         self.build_self()
@@ -34,8 +35,8 @@ class ClazzSummarySection(StudentReportSection):
         self.tally = data_manager.PaperMultiScoreTally.from_clazz(self.clazz, self.paper)
 
 class CohortSummarySection(StudentReportSection):
-    def __init__(self, html_macro, paper):
-        self.html_macro = html_macro
+    def __init__(self, html_macro, student, clazz, paper):
+        self.html_macro = 'student/report_section_macros/cohort_summary.html'
         self.paper = paper
         self.build_self()
 
@@ -45,8 +46,8 @@ class CohortSummarySection(StudentReportSection):
 ## Strengths and Weaknesses = Highlights
 
 class HighlightsSection(StudentReportSection):
-    def __init__(self, html_macro, student, paper):
-        super().__init__(html_macro, student, paper)
+    def __init__(self, html_macro, student, clazz, paper):
+        super().__init__('student/report_section_macros/highlights_section.html', student, clazz, paper)
 
     def build_self(self):
         raw_scores = models.Score.query.filter_by(student_id = self.student.id, paper_id = self.paper.id).all()
