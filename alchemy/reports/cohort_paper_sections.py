927e4d6a3d7ca2d707bd194464c94f4932239e5a
RobinT <the5thforce@protonmail.com>
2021-09-07 17:25:50 +0700

    Adjusted the existing cohort report sections with the kwargs structure. Working like a charm!

--- alchemy/reports/cohort_paper_sections.py
+++ alchemy/reports/cohort_paper_sections.py
@@ -2,35 +2,36 @@ from alchemy import models
 from alchemy.reports import data_manager
 
 class CohortReportSection:
-    def __init__(self, html_macro, paper):
+    def __init__(self, html_macro, **kwargs):
         self.html_macro = html_macro
-        self.paper = paper
+        for key, value in kwargs.items():
+            setattr(self, key, value)
         self.build_self()
 
 class OverviewSection(CohortReportSection):
-    def __init__(self, html_macro, paper):
-        super().__init__(html_macro, paper)
+    def __init__(self, **section_kwargs):
+        super().__init__('course/cohort/report_section_macros/overview.html', **section_kwargs)
 
     def build_self(self):
          self.tally = data_manager.PaperMultiScoreTally.from_cohort(self.paper)
 
 class OverviewPlotSection(CohortReportSection):
-    def __init__(self, html_macro, paper):
-        super().__init__(html_macro, paper)
+    def __init__(self, **section_kwargs):
+        super().__init__('course/cohort/report_section_macros/overview_plot.html', **section_kwargs)
 
     def build_self(self):
         self.plot = data_manager.create_cohort_distribution_plot(self.paper)
 
 class OverviewDetailsSection(CohortReportSection):
-    def __init__(self, html_macro, paper):
-        super().__init__(html_macro, paper)
+    def __init__(self, **section_kwargs):
+        super().__init__('course/cohort/report_section_macros/overview_details.html', **section_kwargs)
 
     def build_self(self):
         self.statprofile = data_manager.StatProfile(data_manager.total_student_scores_for_cohort(self.paper), self.paper.profile.total_points)
 
 class GradeOverviewSection(CohortReportSection):
-    def __init__(self, html_macro, paper):
-        super().__init__(html_macro, paper)
+    def __init__(self, **section_kwargs):
+        super().__init__('course/cohort/report_section_macros/grades_overview.html', **section_kwargs)
 
     def build_self(self):
         clazzes = self.paper.course.clazzes
--- alchemy/reports/report_types.py
+++ alchemy/reports/report_types.py
@@ -37,56 +37,21 @@ class ClazzPaperReport(object):
             section = section_class(paper = self.paper, clazz = self.clazz)
             self.sections.append(section)
 
-    # def old_build_self(self):
-    #     self.title = f"{self.clazz.code} - Achievment Report"
-    #     self.subtitle = f"{self.clazz.course.name}: {self.paper.title}"
-    #
-    #     overview_section = clazz_paper_sections.OverviewSection('course/clazz/report_section_macros/overview.html', self.clazz, self.paper)
-    #     self.sections.append(overview_section)
-    #
-    #     overview_plot_section = clazz_paper_sections.OverviewPlotSection('course/clazz/report_section_macros/overview_plot.html', self.clazz, self.paper)
-    #     self.sections.append(overview_plot_section)
-    #
-    #     overview_details_section = clazz_paper_sections.OverviewDetailsSection('course/clazz/report_section_macros/overview_details.html', self.clazz, self.paper)
-    #     self.sections.append(overview_details_section)
-    #
-    #     grade_overview_section = clazz_paper_sections.GradeOverviewSection('course/clazz/report_section_macros/grades_overview.html', self.clazz, self.paper)
-    #     self.sections.append(grade_overview_section)
-    #
-    #     tag_overview_section = clazz_paper_sections.TagOverviewSection('course/clazz/report_section_macros/tag_overview.html', self.clazz, self.paper)
-    #     self.sections.append(tag_overview_section)
-    #
-    #     question_overview_section = clazz_paper_sections.QuestionOverviewSection('course/clazz/report_section_macros/question_overview.html', self.clazz, self.paper)
-    #     self.sections.append(question_overview_section)
-    #
-    #     tag_details_section = clazz_paper_sections.TagDetailsSection('course/clazz/report_section_macros/tag_details.html', self.clazz, self.paper)
-    #     self.sections.append(tag_details_section)
-    #
-    #     question_details_section = clazz_paper_sections.QuestionDetailsSection('course/clazz/report_section_macros/question_details.html', self.clazz, self.paper)
-    #     self.sections.append(question_details_section)
-
 class CohortPaperReport(object):
-    def __init__(self, paper):
+    def __init__(self, paper, section_types = ['OverviewSection', 'OverviewPlotSection', 'OverviewDetailsSection', 'GradeOverviewSection']):
         self.title = None
         self.subtitle = None
         self.clazzes = []
         self.paper = paper
         self.sections = []
-        self.build_self()
+        self.build_self(section_types)
 
-    def build_self(self):
+    def build_self(self, section_types):
         self.title = f"{self.paper.course.name} - Cohort Achievment Report"
         self.subtitle = f"Assessment: {self.paper.title}"
         self.clazzes = [clazz for clazz in self.paper.course.clazzes]
 
-        overview_section = cohort_paper_sections.OverviewSection('course/cohort/report_section_macros/overview.html', self.paper)
-        self.sections.append(overview_section)
-
-        overview_plot_section = cohort_paper_sections.OverviewPlotSection('course/cohort/report_section_macros/overview_plot.html', self.paper)
-        self.sections.append(overview_plot_section)
-
-        overview_details_section = cohort_paper_sections.OverviewDetailsSection('course/cohort/report_section_macros/overview_details.html', self.paper)
-        self.sections.append(overview_details_section)
-
-        grade_overview_section = cohort_paper_sections.GradeOverviewSection('course/cohort/report_section_macros/grades_overview.html', self.paper)
-        self.sections.append(grade_overview_section)
+        for section_type in section_types:
+            section_class = getattr(cohort_paper_sections, section_type)
+            section = section_class(paper = self.paper)
+            self.sections.append(section)
