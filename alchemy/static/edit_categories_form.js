63dde519122239dfbab9dfc8d7620d7f8df25302
RobinT <the5thforce@protonmail.com>
2022-02-02 12:26:31 +0700

    Fixed up the edit_categories.js and now the validator functions seem to be working correctly, even when new rows are added.

--- alchemy/static/edit_categories_form.js
+++ alchemy/static/edit_categories_form.js
@@ -1,5 +1,4 @@
 function validate_categories(course_id){
-  console.log("Made it to validate_categories!")
   var category_ids = []
   var category_names = []
   var weightings = []
@@ -25,7 +24,6 @@ function validate_categories(course_id){
   if (!validate_form(category_names, weightings)) {
     return false
   }
-  console.log(categories)
   $.ajax({
     type: 'POST',
     url: '/course/' + course_id + '/edit_categories',
@@ -68,13 +66,13 @@ function check_weightings_sum(weightings_list){
   var weighting_sum = 0
   for (i=0; i<weightings_list.length; i++) {
     weighting_sum += weightings_list[i]
-    if (weighting_sum > 100){
-      var input = document.getElementById('weighting_'+i)
-      console.log("Indexed Inputs: ", input)
-      input.classList.add('is-invalid')
-      var feedback = document.getElementById('weighting_error_'+i)
-      feedback.innerHTML = 'Weightings must sum to 100%'
-      return false
+
+  if (weighting_sum > 100){
+    var input = document.getElementById('weighting_'+i)
+    input.classList.add('is-invalid')
+    var feedback = document.getElementById('weighting_error_'+i)
+    feedback.innerHTML = 'Weightings must sum to 100%'
+    return false
     }
   }
   if (weighting_sum <100){
@@ -108,22 +106,24 @@ function new_hidden_id_field(){
   return id_field
 }
 
-function new_category_field(){
-  var grade_field = document.createElement('INPUT')
-  grade_field.type = 'text'
-  grade_field.className = 'form-control'
-  grade_field.placeholder = 'Category'
-  grade_field.setAttribute('required', '')
-  return grade_field
+function new_category_field(id){
+  var category_field = document.createElement('INPUT')
+  category_field.type = 'text'
+  category_field.className = 'form-control'
+  category_field.placeholder = 'Category'
+  category_field.setAttribute('required', '')
+  category_field.id = 'category_'+id
+  return category_field
 }
 
-function new_weighting_field(){
-  var lb_field = document.createElement('INPUT')
-  lb_field.type = 'float'
-  lb_field.className = 'form-control'
-  lb_field.placeholder = 'Weighting'
-  lb_field.setAttribute('required', '')
-  return lb_field
+function new_weighting_field(id){
+  var weight_field = document.createElement('INPUT')
+  weight_field.type = 'float'
+  weight_field.className = 'form-control'
+  weight_field.placeholder = 'Weighting'
+  weight_field.setAttribute('required', '')
+  weight_field.id = 'weighting_'+id
+  return weight_field
 }
 
 function new_delete_button(){
@@ -142,7 +142,31 @@ function create_trash_icon(){
   return span
 }
 
+function new_category_error_label(id){
+  var label = document.createElement('LABEL')
+  label.className = 'invalid-feedback'
+  label.id = 'category_error_'+id
+  label.htmlFor = "category_"+id
+  return label
+}
+
+function new_weighting_error_label(id){
+  var label = document.createElement('LABEL')
+  label.className = 'invalid-feedback'
+  label.id = 'weighting_error_'+id
+  label.htmlFor = "weighting_"+id
+  return label
+}
+
 function insert_cat_field_row(){
+  var table_length = 0
+  var i = 0
+  $('#category-form-table').find('input').each(function(){
+    if (i % 3 == 0) {
+      table_length += 1
+    }
+    i++
+  })
   var table = document.getElementById('category-form-table')
   var row = table.insertRow(-1)
   var cell0 = row.insertCell(0)
@@ -151,8 +175,10 @@ function insert_cat_field_row(){
   var cell3 = row.insertCell(3)
   cell0.appendChild(new_hidden_id_field())
   cell0.style = "display:none"
-  cell1.appendChild(new_category_field())
-  cell2.appendChild(new_weighting_field())
+  cell1.appendChild(new_category_field(table_length))
+  cell1.appendChild(new_category_error_label(table_length))
+  cell2.appendChild(new_weighting_field(table_length))
+  cell2.appendChild(new_weighting_error_label(table_length))
   var btn = new_delete_button()
   var span = create_trash_icon()
   cell3.appendChild(btn)
