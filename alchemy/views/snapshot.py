150b18f8befd2f62cc784cc02d1bd99a898e2199
RobinT <the5thforce@protonmail.com>
2022-02-19 15:52:26 +0700

    Made some adjustments to course index that I think make more sense for the addition of snapshots. Also added a snapshot.py file which currently doesn't contain much other than a description of what the take_snapshot function needs to do.

---
+++ alchemy/views/snapshot.py
@@ -0,0 +1,26 @@
+## Temporary place for designing the Snapshot functionality ##
+
+class Snapshot(object):
+    def __init__(self, name, course, paper_list):
+        self.name = name
+        self.course = course
+        self.papers = paper_list
+
+# def take_snapshot(name, paper_list):
+
+    # Back end:
+    # 1) Calculate culumative marks for individual students
+    #
+    # 2) Prepare other values / data sets for use in analysing individual students (graphs, report sections, etc)
+    #
+    # 3) Calculate average achievement for cohort/clazzes
+    #
+    # 4) Prepare other values / data sets for use in analysing cohort/clazzes (graphs, report sections, etc)
+
+    # Front End
+    # 
+    # 1) Update table on Course indexing
+    #
+    # 2) Make new columns in cohort / clazz student lists w/ grades and report buttons
+    #
+    # 3) Add entry to some sort of table on Cohort index
