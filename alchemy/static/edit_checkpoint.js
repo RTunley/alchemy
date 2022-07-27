c0a396991c875bda2b050416114356d7542e0d8b
RobinT <the5thforce@protonmail.com>
2022-07-27 16:32:09 -0500

    Fixed up the edit_checkpoint.js functions so that now the edit checkpoint form appears to behave correctly. Next step is to add actual adjustments to the checkpoint papers list through the selections.

--- alchemy/static/edit_checkpoint.js
+++ alchemy/static/edit_checkpoint.js
@@ -8,6 +8,31 @@ all_switch.addEventListener('change', function() {
   }
 );
 
+function get_paper_switches(container){
+  let switches = []
+  container.find('input').each(function(){
+    let paper_switch = $(this).get(0)
+    if (paper_switch.id != 'all_papers_switch') {
+      switches.push(paper_switch)
+    }
+  })
+  return switches
+}
+
+paper_switch_list = get_paper_switches(switch_container)
+for (let i = 0; i < paper_switch_list.length; i++) {
+  paper_switch_list[i].addEventListener('change', function() {
+    let all_checked = true
+    switches = get_paper_switches(switch_container)
+    for (let i = 0; i < paper_switch_list.length; i++) {
+      if (!switches[i].checked) {
+        all_checked = false
+      }
+    }
+    all_switch.checked = all_checked
+  })
+}
+
 function get_paper_ids(course_id, checkpoint_id) {
   $.ajax({
     type: 'GET',
@@ -38,57 +63,3 @@ function initialize_switches(paper_id_list){
   })
   all_switch.checked = set_all_switch
 }
-
-
-// switch_container.find('input').each(function () {
-//   let paper_switch = $(this).get(0)
-//   paper_switch.addEventListener('change', function() {
-//     let all_checked = true
-//     switch_container.find('input').each(function () {
-//         let paper_switch_loop = $(this).get(0)
-//         if (!paper_switch_loop.checked) {
-//           all_checked = false
-//         }
-//     })
-//     if (all_checked == true) {
-//       all_switch.checked = true
-//     }
-//   })
-// )}
-
-//
-// function checkpoint_papers_all() {
-//   // Activate all papers
-//   checkpoint_papers_reset(true)
-// }
-//
-// function checkpoint_papers_none() {
-//   // Deactivate all papers
-//   checkpoint_papers_reset(false)
-// }
-//
-// function checkpoint_papers_reset(activate_papers) {
-//   // Reset the list of papers
-//   var switch_container = $('#edit_checkpoint_switch_container')
-//   if (activate_papers || !window.checkpoint_paper_select) {
-//     window.checkpoint_paper_select = {}
-//   }
-//
-//   // Reset the active state of all paper switches
-//   switch_container.children('input').each(function () {
-//     var switch = $(this)
-//     switch.toggleClass('active', activate_papers)
-//     if (!activate_papers) {
-//       var paper_id = button.attr('data-alchemy_paper_id')
-//       window.checkpoint_paper_select[paper_id] = true
-//     }
-//   })
-  //
-  // if (activate_tags) {
-  //   $('#library_question_filter_tags_all').removeAttr('href')
-  //   $('#library_question_filter_tags_none').attr('href', '#')
-  // } else {
-  //   $('#library_question_filter_tags_all').attr('href', '#')
-  //   $('#library_question_filter_tags_none').removeAttr('href')
-  // }
-// }
