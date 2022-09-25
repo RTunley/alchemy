3b2ba1cb8c8d5d32d057d75f2d36be5f3b724c5e
RobinT <the5thforce@protonmail.com>
2022-09-25 18:53:51 +0700

    Added the How? dropdown so students and teachers can see how their final grade was calculated, and added the adjacent grades section to the checkpoint report.

--- alchemy/reports/checkpoint_data_manager.py
+++ alchemy/reports/checkpoint_data_manager.py
@@ -12,6 +12,35 @@ class StudentCheckpointTally():
         self.grade = None
         self.paper_score_tally_list = []
         self.category_tally_groups = []
+        self.weight_sum = 0
+        self.build_self(student, checkpoint)
+
+    def build_self(self, student, checkpoint):
+
+        # Make StudentPaperTally for each paper
+        for paper in checkpoint.papers:
+            paper_tally = data_manager.PaperScoreTally.from_student(student, paper)
+            self.paper_score_tally_list.append(paper_tally)
+
+        # Group tallys into PaperTallyGroups and find weight_sum
+        # (in case some categories are missing - normally weight_sum = 100)
+
+        all_categories = [category for category in checkpoint.course.assessment_categories]
+        for category in all_categories:
+            category_group = CategoryTallyGroup(category, self.paper_score_tally_list)
+            self.category_tally_groups.append(category_group)
+            if category_group.percentage != None:
+                self.weight_sum += category_group.category.weight
+
+        # Add up the weighted category averages and normalize against weight_sum
+        weighted_category_sum = 0
+        for group in self.category_tally_groups:
+            if group.percentage != None:
+                group.get_weighted_percentage(self.weight_sum)
+                weighted_category_sum += round(group.percentage*group.category.weight, 2)
+
+        self.percentage = round(weighted_category_sum/self.weight_sum, 1)
+        self.grade = data_manager.determine_grade(self.percentage, checkpoint.course)
 
 class CategoryTallyGroup():
     def __init__(self, category, paper_tally_list):
@@ -20,6 +49,7 @@ class CategoryTallyGroup():
         self.total_points = 0
         self.total_tally = 0
         self.percentage = 0
+        self.weighted_percentage = 0
         self.build_self(paper_tally_list)
 
     def build_self(self, paper_tally_list):
@@ -36,33 +66,34 @@ class CategoryTallyGroup():
         else:
             self.percentage = None
 
-def make_student_checkpoint_tally(student, checkpoint):
-    student_checkpoint_tally = StudentCheckpointTally(student, checkpoint)
+    def get_weighted_percentage(self, weight_sum):
+        self.weighted_percentage = round(self.total_tally/self.total_points*self.category.weight/weight_sum*100, 2)
 
-    # Make StudentPaperTally for each paper
-    for paper in checkpoint.papers:
-        paper_tally = data_manager.PaperScoreTally.from_student(student, paper)
-        student_checkpoint_tally.paper_score_tally_list.append(paper_tally)
+class AdjacentGrades(object):
+    def __init__(self, grade_list, percentage, grade):
+        self.higher_grade = None
+        self.diff_higher_grade = 0
+        self.lower_grade = None
+        self.diff_lower_grade = 0
+        self.build_self(grade_list, percentage, grade)
 
-    # Group tallys into PaperTallyGroups and find weight_sum
-    # (in case some categories are missing - normally weight_sum = 100)
-
-    all_categories = [category for category in checkpoint.course.assessment_categories]
-    weight_sum = 0
-    for category in all_categories:
-        category_group = CategoryTallyGroup(category, student_checkpoint_tally.paper_score_tally_list)
-        student_checkpoint_tally.category_tally_groups.append(category_group)
-        if category_group.percentage != None:
-            weight_sum += category_group.category.weight
-
-    # Add up the weighted category averages and normalize against weight_sum
-    weighted_category_sum = 0
-    for group in student_checkpoint_tally.category_tally_groups:
-        print(group.category.name)
-        print(group.percentage)
-        if group.percentage != None:
-            weighted_category_sum += round(group.percentage*group.category.weight, 2)
-
-    student_checkpoint_tally.percentage = round(weighted_category_sum/weight_sum, 1)
-    student_checkpoint_tally.grade = data_manager.determine_grade(student_checkpoint_tally.percentage, checkpoint.course)
-    return student_checkpoint_tally
+    def build_self(self, grade_list, percentage, grade):
+        for i in range(len(grade_list)):
+            if grade_list[i].grade == grade:
+                index = i
+                break
+        if index == 0:
+            self.lower_grade = grade_list[index+1]
+            self.diff_lower_grade = round(percentage - self.lower_grade.upper_bound, 1)
+            self.higher_grade = None
+            self.diff_higher_grade = None
+        elif index == len(grade_list)-1:
+            self.higher_grade = grade_list[index-1]
+            self.diff_higher_grade = round(self.higher_grade.lower_bound - percentage, 1)
+            self.lower_grade = None
+            self.diff_lower_grade = None
+        else:
+            self.higher_grade = grade_list[index-1]
+            self.diff_higher_grade = round(self.higher_grade.lower_bound - percentage, 1)
+            self.lower_grade = grade_list[index+1]
+            self.diff_lower_grade = round(percentage - self.lower_grade.upper_bound, 1)
--- alchemy/reports/student_checkpoint_sections.py
+++ alchemy/reports/student_checkpoint_sections.py
@@ -13,4 +13,12 @@ class OverviewSection(StudentReportSection):
         super().__init__('student/checkpoint_report_sections/overview.html', **section_kwargs)
 
     def build_self(self):
-        self.checkpoint_tally = checkpoint_data_manager.make_student_checkpoint_tally(self.student, self.checkpoint)
+        self.checkpoint_tally = checkpoint_data_manager.StudentCheckpointTally(self.student, self.checkpoint)
+
+class AdjacentGradesSection(StudentReportSection):
+    def __init__(self, **section_kwargs):
+        super().__init__('student/checkpoint_report_sections/adjacent_grades.html', **section_kwargs)
+
+    def build_self(self):
+        checkpoint_tally = checkpoint_data_manager.StudentCheckpointTally(self.student, self.checkpoint)
+        self.adjacent_grades = checkpoint_data_manager.AdjacentGrades(self.checkpoint.course.grade_levels, checkpoint_tally.percentage, checkpoint_tally.grade)
